642b5f1b18adb290d698b20fc272150da64c9070
Felipe Santiago Sandoval Moreno <felipesantiagosandoval@gmail.com>
2019-08-11 20:46:19 -0500

    add apollo

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
 import express from 'express';
-import graphqlHTTP from 'express-graphql';
-import schema from './schema';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './data/schema';
+import { resolvers } from './data/resolvers';
 
 const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.get('/', (req, res) => {
-	res.send('Todo Listo');
-});
-
-//resolver
-const root = {
-	cliente: () => {
-		return {
-			id: 123424,
-			nombre: 'Santiago',
-			apellido: 'Sandoval',
-			empresa: 'Quick',
-			emails: [
-				{
-					email: 'felipesantiagosandoval@gmail.com',
-					email: 'santiago@gmail.com'
-				}
-			]
-		};
-	}
-};
-app.use(
-	'/grapqhl',
-	graphqlHTTP({
-		schema,
-		rootValue: root,
-		graphiql: true
-	})
+server.applyMiddleware({ app });
+app.listen({ port: 4000 }, () =>
+	console.log(`El servidor esta corriendo en http://localhost:4000${server.graphqlPath}`)
 );
-
-app.listen(8000, () => {
-	console.log('El servidor esta funcionando');
-});
